32960fceb269fd48b8c9697589c9d5765fb63055
Matthew Forsyth <matt@mojotech.com>
2014-05-29 11:10:37 -0400

    Safer animate.js

--- animate.js
+++ animate.js
@@ -4,9 +4,15 @@ var ar = require("nodecopter"),
 
 client.takeoff();
 console.log("took off");
-client.animate('flipBehind', 5000);
-console.log("animated");
-client.after(3000, function() {
+
+client
+  .after(5000, function() {
+    console.log("animating");
+    this.animate('yawShake', 5000);
+    this.clockwise(0.5);
+  })
+  .after(30000, function() {
+    console.log("stopping/landing");
     this.stop();
     this.land();
   });
